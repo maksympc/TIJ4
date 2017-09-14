e331de5eaa1a1f7ea170850333a6038664ba14d1
Maksym Pinchuk <maksympc@gmail.com>
2017-09-14 23:03:59 +0300

    add HelloDate class, first class in TIJ4 project, contains logic to say hello and print current date

---
+++ src/com/tij4/pinchuk/chapter2/HelloDate.java
@@ -0,0 +1,14 @@
+package com.tij4.pinchuk.chapter2;
+
+import java.util.Date;
+
+/**
+ * This {@link HelloDate class} contains logic to say hello and print current day for user.
+ * */
+public class HelloDate {
+
+    public static void main(String[] args ){
+        System.out.println("Привет, дорогой друг, сегодня: " + new Date());
+    }
+
+}
